4fecd6940246bad260c4beb824b6da0a9dc147d2
renaealisha54-debug <renaealisha54@gmail.com>
2026-04-20 03:53:48 -0400

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.vicious.overlay"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.vicious.overlay"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
