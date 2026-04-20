a8811866537e28db35f60e002d31d7257437e031
renaealisha54-debug <renaealisha54@gmail.com>
2026-04-20 04:09:03 -0400

    Create MainActivity.kt

---
+++ app/src/main/java/com/vicious/overlay/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.vicious.overlay
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!Settings.canDrawOverlays(this)) {
+            startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")))
+        } else {
+            startService(Intent(this, ViciousOverlayService::class.java))
+            finish()
+        }
+    }
+}
