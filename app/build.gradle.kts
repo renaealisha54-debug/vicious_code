a876246cf01659c8fd03ee836a58e8e9f30471e7
renaealisha54-debug <renaealisha54@gmail.com>
2026-04-19 23:43:58 -0400

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,18 @@
+// Full Name: app/build.gradle.kts
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.vicious.viciouslayer"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.vicious.viciouslayer"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
